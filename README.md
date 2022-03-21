7aaa48ae3b6d14ff1374ce156f79a60197442aff
jalbertomr <jalbertomr@yahoo.com>
2022-03-21 10:40:57 -0600

    SpringBoot Application HomeController

---
+++ README.md
@@ -0,0 +1,4 @@
+#Testing the Web Layer
+## Spring boot from https://spring.io/guides/gs/testing-web/
+- SpringBoot Application HomeController
+- Simple sanity check to check application context loads
---
+++ src/main/java/com/bext/testing/TestingWebApplication.java
@@ -0,0 +1,11 @@
+package com.bext.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TestingWebApplication.class);
+    }
+}
