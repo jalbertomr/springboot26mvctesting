86f26e6917ca7e02ac32307e873eb0a9a33bff28
jalbertomr <jalbertomr@yahoo.com>
2022-03-21 10:45:20 -0600

    Simple sanity check to check application context loads @SpringBootTest

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## Spring boot from https://spring.io/guides/gs/testing-web/
 - SpringBoot Application HomeController
 - Simple sanity check to check application context loads
+  @SpringBootTest
---
+++ src/test/java/com/bext/testing/TestingWebApplicationTest.java
@@ -0,0 +1,13 @@
+package com.bext.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TestingWebApplicationTest {
+
+    @Test
+    public void contextLoads(){}
+}
